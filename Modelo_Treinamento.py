5c4f758906ff9d92a576445f3570322aa6c4078d
unzendake <samuelromancini34@gmail.com>
2025-10-24 19:52:14 +0000

    first steps to bild a machime learning model

---
+++ Modelo_Treinamento.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import joblib
+import os 
+from sklearn import model_selection, preprocessing, pipeline, linear_model, metrics
+
+# carregar dados
+
+def carregar_dados(caminho_arquivo = "historicoAcademico.csv"):
+
+    try:
+        if os.path.exists(caminho_arquivo):
+
+            df = pd.read_csv(caminho_arquivo, encoding="latin1", sep=",")
+
+            print("O arquivo foi carregado com sucesso!")
+
+            return df
+        
+        else:
+
+            print("O arquivo não foi encontrado dentro da pasta!")
+
+            return None
+        
+    except Exception as e:
+
+        print("Erro inesperado ao carregar o arquivo: ", e)
+
+        return None
+
+
+dados = carregar_dados()
